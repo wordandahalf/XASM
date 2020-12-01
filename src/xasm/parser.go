522ee7846a527c331c9f5c49eef70c0a58dee49d
Ryan Jones <mail2ryanjones@gmail.com>
2020-11-30 23:33:30 -0600

    Started implementing more advanced mnemonic parsing

--- src/xasm/parser.go
+++ src/xasm/parser.go
@@ -25,6 +25,11 @@ type xasmInstruction struct {
     operands    []string
 }
 
+type xasmOperand struct {
+    operandType string
+    value       interface{}
+}
+
 func check(e error) {
     if e != nil {
         panic(e)
@@ -83,8 +88,37 @@ var (
     spacePattern, _         = regexp.Compile("[[:blank:]]{2,}")
 
     labelPattern, _         = regexp.Compile("^([a-zA-Z_]+)" + labelSuffix + "$")
-    instructionPattern, _   = regexp.Compile("^([a-zA-Z]+)(?: |$)")
-    opcodePattern, _        = regexp.Compile("^\\s*\\w+\\s*$")
+    instructionPattern, _ = regexp.Compile("^([a-zA-Z]+)(?: |$)")
+    operandPattern, _     = regexp.Compile("^\\s*\\w+\\s*$")
+
+    // Special parsers for instructions that require further
+    // processing of the mnemonic and operands.
+    instructionParsers      = map[string]func(string, []string) xasmInstruction {
+        "JP": parseJump,
+        "JZ": parseJump,
+        "JC": parseJump,
+
+        "NOT":  parseAlu,
+        "AND":  parseAlu,
+        "OR":   parseAlu,
+        "XOR":  parseAlu,
+        "SHL":  parseAlu,
+        "SHR":  parseAlu,
+        "ADD":  parseAlu,
+        "SUB":  parseAlu,
+    }
+
+    // Maps mnemonics to ALU opcodes.
+    aluOpcodes              = map[string]byte {
+        "NOT":  0b000,
+        "AND":  0b001,
+        "OR":   0b010,
+        "XOR":  0b011,
+        "SHL":  0b100,
+        "SHR":  0b101,
+        "ADD":  0b110,
+        "SUB":  0b111,
+    }
 )
 
 func parseLine(line xasmLine) xasmInstruction {
@@ -134,26 +168,45 @@ func parseLine(line xasmLine) xasmInstruction {
 
         text := strings.Replace(text, instructionMatches[0], "", 1)
 
-        instruction = xasmInstruction{instructionMatches[1], []string {}}
+        mnemonic := instructionMatches[1]
+        operands := make([]string, 0)
 
         if text != "" {
-            opcodes := strings.Split(text, ",")
+            rawOperands := strings.Split(text, ",")
 
-            for _, opcode := range opcodes {
-                if opcodePattern.MatchString(opcode) {
-                    instruction.operands = append(instruction.operands, strings.TrimSpace(opcode))
+            for _, operand := range rawOperands {
+                if operandPattern.MatchString(operand) {
+                    operands = append(operands, strings.TrimSpace(operand))
                 } else {
                     // This accounts for malformed operands with multiple spaces in between.
                     // Since the parser replaces multiple spaces with single spaces,
                     // a simple strings.Index call with the provided operand could result in
                     // -1 being returned.
-                    index := strings.Index(line.content, strings.Split(strings.TrimSpace(opcode), " ")[0])
+                    index := strings.Index(line.content, strings.Split(strings.TrimSpace(operand), " ")[0])
 
                     log.Fatalf( "Syntax error on line %d: malformed operand\n%s\n%s^\n", line.number, line.content, strings.Repeat(" ", index))
                 }
             }
         }
+
+        parser, e := instructionParsers[mnemonic]
+
+        if e {
+            instruction = parser(mnemonic, operands)
+        } else {
+            instruction = xasmInstruction{mnemonic, operands}
+        }
     }
 
     return instruction
+}
+
+func parseJump(mnemonic string, operands []string) xasmInstruction {
+    return xasmInstruction{ "J", append([]string { strings.TrimPrefix(mnemonic, "J") }, operands...)}
+}
+
+func parseAlu(mnemonic string, operands []string) xasmInstruction {
+    aluOpcode := aluOpcodes[mnemonic]
+
+    return xasmInstruction{"ALU", append([]string { string(0x30 + aluOpcode) }, operands...)}
 }
